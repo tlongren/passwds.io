081fced7f61f483681d663aaf85a3414cde81b15
Tyler Longren <tlongren@gmail.com>
2017-08-30 11:52:05 -0700

    Update fontawesome.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (extension_loaded('newrelic')) {
 <title>passwds.io - The Pronounceable Password Generator</title>
 <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="js/jquery.prettySocial.min.js"></script>
